cfb905a259b9d211b5ae877210eb5ece6907d44b
Jessica Howkins <jess.howkins@yahoo.com>
2025-01-08 19:37:07 +0000

    added post request in views.py

--- about/views.py
+++ about/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def about_me(request):
+    return HttpResponse("This would be the about page")
+
+def index(request):
+   if request.method == "GET":
+       return HttpResponse("This was a GET request")
+   elif request.method == "POST":
+       return HttpResponse("You must have POSTed something")
+       else: 
+        return 
+    HttpResponse(request.method)
