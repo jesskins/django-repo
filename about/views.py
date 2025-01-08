4c299cd69ae0ebd13fd7a4a7dc22a78102357726
Jessica Howkins <jess.howkins@yahoo.com>
2025-01-08 20:29:13 +0000

    file corrupted, found old version and ammended with new

--- about/views.py
+++ about/views.py
@@ -6,10 +6,9 @@ def about_me(request):
     return HttpResponse("This would be the about page")
 
 def index(request):
-   if request.method == "GET":
-       return HttpResponse("This was a GET request")
-   elif request.method == "POST":
-       return HttpResponse("You must have POSTed something")
-       else: 
-        return 
-    HttpResponse(request.method)
+    if request.method == "GET":
+        return HttpResponse("This was a GET request")
+    elif request.method == "POST":
+        return HttpResponse("You must have POSTed something")
+    else:
+        return HttpResponse(f"This was a {request.method} request")
